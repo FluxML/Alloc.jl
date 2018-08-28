23046d43434d1b70e8d291c16124c5d808c411f4
Mike J Innes <mike.j.innes@gmail.com>
2018-08-28 16:57:15 +0100

    staticalloc -> alloc

--- src/StaticAlloc.jl
+++
@@ -1,9 +0,0 @@
-module StaticAlloc
-
-using Cassette
-import Cassette: @context, overdub, execute, prehook
-
-include("profile.jl")
-include("alloc.jl")
-
-end # module
--- src/alloc.jl
+++ src/alloc.jl
@@ -1,32 +1,9 @@
-using Base: unsafe_convert
+module Alloc
 
-mutable struct Buffer
-  buf::Vector{UInt8}
-  offset::UInt
-end
+using Cassette
+import Cassette: @context, overdub, execute, prehook
 
-Buffer(n::Int) = Buffer(Vector{UInt8}(undef, n),0)
+include("profile.jl")
+include("stack.jl")
 
-function alloc(b::Buffer, ::Type{Array{T,N}}, d::NTuple{N,Int}) where {T,N}
-  # @info "Allocating $(prod(d)) * $(T)"
-  ptr = unsafe_convert(Ptr{UInt8}, b.buf) + b.offset
-  b.offset += sizeof(T) * prod(d)
-  b.offset > length(b.buf) && error("StaticAlloc: Out of memory")
-  unsafe_wrap(Array, convert(Ptr{T}, ptr), d)
-end
-
-function clear!(b::Buffer)
-  b.offset = 0
-  return b
-end
-
-@context AllocCtx
-
-execute(cx::AllocCtx,
-    ::Type{Array{T,N}}, ::UndefInitializer, d::Vararg{Int,N}) where {T,N} =
-  alloc(cx.metadata, Array{T,N}, d)
-
-function run(f, b::Buffer)
-  clear!(b)
-  overdub(AllocCtx(metadata = b), f)
-end
+end # module
